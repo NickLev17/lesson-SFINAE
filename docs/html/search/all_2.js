e19bdda97d0674672a255a3162f9ed9be537ebcb
NickLev17 <LevenkovNick17@yandex.ru>
2025-07-02 19:57:40 +0300

    Delete docs/html directory

--- docs/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['print_5fip_0',['print_ip',['../print__ip_8cpp.html#a643167f9ce61dfc137976fd6fd875c85',1,'print_ip(const T &amp;value):&#160;print_ip.cpp'],['../print__ip_8cpp.html#adedd19b17f027f18acf5ebdc0abe3aa3',1,'print_ip(const T &amp;value):&#160;print_ip.cpp'],['../print__ip_8cpp.html#aab768da97bde00cd124c5cb424a4b935',1,'print_ip(const T &amp;value):&#160;print_ip.cpp'],['../print__ip_8cpp.html#a5dc7f149cc2d03d15485b85566a2c70c',1,'print_ip(const T &amp;value):&#160;print_ip.cpp']]],
-  ['print_5fip_2ecpp_1',['print_ip.cpp',['../print__ip_8cpp.html',1,'']]],
-  ['print_5ftuple_5fimpl_2',['print_tuple_impl',['../print__ip_8cpp.html#a1c1fe993271ae793f4151e9cf33937b3',1,'print_ip.cpp']]]
-];
--- docs/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main_0',['main',['../print__ip_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'print_ip.cpp']]]
-];
