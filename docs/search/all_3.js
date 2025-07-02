7350eef8d87d3fe2adae733a5341bba6ce1b1d6f
NickLev17 <LevenkovNick17@yandex.ru>
2025-07-02 20:02:18 +0300

    Add files via upload

---
+++ docs/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value_0',['value',['../structis__tuple_3_01std_1_1tuple_3_01Args_8_8_8_01_4_00_01T_01_4.html#a23a3e65d5c5ec20531f96a3b0e4aee1a',1,'is_tuple&lt; std::tuple&lt; Args... &gt;, T &gt;']]]
+];
